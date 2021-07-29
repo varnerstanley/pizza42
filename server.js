014bdc259c32d3faa240d032ffb02c17aa70b537
Stanley <stanley.e.varner@gmail.com>
2021-07-29 11:25:45 -0600

    Working

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.static(join(__dirname, "public")));
 
+app.use(express.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(express.json())
+
+
 // Create the JWT validation middleware
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -28,31 +34,35 @@ const checkJwt = jwt({
 
 
 
-const checkScopes = jwtAuthz(['read:orders']);
+const checkScopes = jwtAuthz(['update:current_user_metadata']);
 // Create an endpoint that uses the above middleware to
 // protect this route from unauthorized requests
-app.get("/api/external", checkJwt, checkScopes, (req, res) => {
-
+app.put("/api/external/:user_id", checkJwt, checkScopes, async (req, res) => {
+  const userId = req.params.user_id;
+  // console.log('behold the user id', req.params.user_id);
 
   var axios = require("axios").default;
   //console.log(req.headers.authorization);
+
+  console.log(req.body);
+
   var options = {
-    method: 'PATCH', url: `https://${authConfig.domain}/api/v2/users/user_id/`,
+    method: 'PATCH', url: `https://${authConfig.domain}/api/v2/users/${userId}`,
     headers: { authorization: req.headers.authorization, 'content-type': 'application/json' },
-    user_metadata: { pizzaName: "Pepperoni Pizza" }
+    data: {
+      user_metadata: { orders: req.body.orders },
+    }
   };
 
-
-  axios.request(options).then(function (response) {
-    // console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
-
-  console.log("I got here.");
-  res.send({
-    msg: "Access token validated!"
-  });
+  try {
+    const response = await axios.request(options)
+    // console.log('booyah i should have worked: ', response);
+    res.send({
+      msg: "Order Received!",
+    });
+  } catch (error) {
+    console.log('Error', error);
+  }
 });
 
 app.get("/auth_config.json", (req, res) => {
